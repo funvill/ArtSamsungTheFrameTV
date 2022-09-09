27bd752e8121545fae29e3fa358ff3316b598b98
Steven Smethurst <funvill@funvill.com>
2022-09-08 17:37:23 -0700

    Added google tracker

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,4 +3,17 @@
     <hr />
     <p>Designed and developed by <a href='https://blog.abluestar.com/'>Steven Smethurst</a><br />© <a href='http://creativecommons.org/licenses/by-sa/4.0/'>Creative Commons Attribution-ShareAlike</a></p>
   </div>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-301766-4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-301766-4');
+</script>
+
+
+
 </template>
