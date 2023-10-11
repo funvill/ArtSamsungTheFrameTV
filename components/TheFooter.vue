bca891f426043a16ce01c52b798121a8058b5de3
Steven Smethurst <funvill@funvill.com>
2023-10-11 00:07:33 -0700

    Updated the footer with new Google Analytics tags

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,16 +4,13 @@
     <p>Designed and developed by <a href='https://blog.abluestar.com/'>Steven Smethurst</a><br />© <a href='http://creativecommons.org/licenses/by-sa/4.0/'>Creative Commons Attribution-ShareAlike</a></p>
   </div>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-301766-4"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-301766-4');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-301766-4');
 </script>
-
-
 
 </template>
